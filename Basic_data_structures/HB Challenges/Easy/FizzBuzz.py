2e5b06b692a9f7c37113bbdd6ac5eea130c1ac82
Kerrie Wong <wongkerrie@gmail.com>
2021-07-17 14:26:37 -0400

    completed

--- Basic_data_structures/HB Challenges/Easy/FizzBuzz.py
+++ Basic_data_structures/HB Challenges/Easy/FizzBuzz.py
@@ -35,3 +35,30 @@ https://fellowship.hackbrightacademy.com/materials/challenges/fizzbuzz/index.htm
 
 # However, this method is unimplemented.
 
+def fizzbuzz():
+    """Count from 1 to 20 in fizzbuzz fashion."""
+
+# # Conditions:
+#     %3--> fizz
+#     %5--> buzz
+#     %3 and /5--> fizzbuzz
+#     from 1-20
+
+# sudo:
+# iterate over the range(1,21)
+# if-elif-elif-else
+# if /3 and /5 -->fizzbuzz
+# if /3--> fizz
+# if /5--> buzz
+# else--> num
+
+for num in range(1,21):
+    if num % 3 == 0:
+        if num % 5 == 0:
+            print('fizzbuzz')
+        else: 
+            print('fizz')
+    elif num % 5 == 0:
+        print('buzz')
+    else:
+        print(num)
