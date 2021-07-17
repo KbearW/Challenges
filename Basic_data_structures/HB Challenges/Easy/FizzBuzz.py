d2bd75f739dea3c925442d47ed81d1a0eb8df2e5
Kerrie Wong <wongkerrie@gmail.com>
2021-07-17 14:18:09 -0400

    init

---
+++ Basic_data_structures/HB Challenges/Easy/FizzBuzz.py
@@ -0,0 +1,37 @@
+https://fellowship.hackbrightacademy.com/materials/challenges/fizzbuzz/index.html#fizzbuzz
+
+# Write a program that counts from 1 to 20 in fizzbuzz fashion.
+
+# To do so, loop from 1 to 20 (inclusive). Each time through, if the number is evenly divisible by 3, say ‘fizz’. If the number is evenly divisible by 5, say ‘buzz’. If the number is evenly divisible by both 3 and 5, say ‘fizzbuzz’. Otherwise, say the number.
+
+# For example:
+
+# >>> fizzbuzz()
+# 1
+# 2
+# fizz
+# 4
+# buzz
+# fizz
+# 7
+# 8
+# fizz
+# buzz
+# 11
+# fizz
+# 13
+# 14
+# fizzbuzz
+# 16
+# 17
+# fizz
+# 19
+# buzz
+
+# We given you a file, fizzbuzz.py, with a method, fizzbuzz:
+
+# def fizzbuzz():
+#     """Count from 1 to 20 in fizzbuzz fashion."""
+
+# However, this method is unimplemented.
+
