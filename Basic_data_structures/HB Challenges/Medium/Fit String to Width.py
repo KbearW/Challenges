ef3ce190a1dc19ef35b968fdaf2137c9d8acc49c
Kerrie Wong <wongkerrie@gmail.com>
2021-07-20 16:44:59 -0400

    completed

--- Basic_data_structures/HB Challenges/Medium/Fit String to Width.py
+++ Basic_data_structures/HB Challenges/Medium/Fit String to Width.py
@@ -25,3 +25,30 @@
 # Your test input will never contain a character limit that is smaller than the largest continuous sequence of non-whitespace characters (however, this is a great edge case to handle as an additional challenge!)
 
 # We’ve provided fit-to-width.py which contains test cases and a function stub for you to complete.
+def fit_to_width(astr, limit):
+    word_list = list(reversed(astr.split(' ')))
+    # print(word_list)
+
+    results = []
+
+    curr_line = []
+    while word_list:
+        word = word_list[-1]
+        if len(' '.join(curr_line + [word])) <= limit:
+            curr_line.append(word_list.pop())
+        else:
+            if curr_line:
+                results.append(' '.join(curr_line))
+            curr_line = []
+        
+    results.append(' '.join(curr_line))
+    print(results)
+
+    for result in results:
+        print(result)
+    # for index in range(0,len(word_list)):
+    #     if len(word) < limit:
+
+    # Notes:
+    # split method --> txt.split(", ")
+    # need to use a loop, and ''.join method
