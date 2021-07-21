c0a796a2e385c0a1cd133674c7437527af0a6d26
Kerrie Wong <wongkerrie@gmail.com>
2021-07-20 21:21:41 -0400

    completed

--- Basic_data_structures/Delete Node in a Linked List.py
+++ Basic_data_structures/Delete Node in a Linked List.py
@@ -33,3 +33,20 @@
 # Input: head = [-3,5,-99], node = -3
 # Output: [5,-99]
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        if node.next != None:
+            node.val = node.next.val
+            node.next = node.next.next
+        else:
+            print('This is out of scope')
