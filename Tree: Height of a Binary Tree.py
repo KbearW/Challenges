6f5846f0e331ec6c311ccce93c37d1a643b21cde
Kerrie Wong <wongkerrie@gmail.com>
2021-07-26 20:15:31 -0400

    completed

--- Tree: Height of a Binary Tree.py
+++ Tree: Height of a Binary Tree.py
@@ -48,3 +48,70 @@
 
 # There are
 # nodes in this path that are connected by edges, meaning our binary tree's .
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+def height(root):
+    if root is None:
+        return -1
+    
+    return max(height(root.left), height(root.right)) + 1
+
+    # Base case:
+    # if Node == None:
+        # return -1
+    # return max(recursive(left), recursive(right)) + 1 
+    # sudo:
+    # condition #1:
+        #node.left != None
+    
+    # condition #2:
+        # node.right != None
+    # count left/ right
+    # recursive function
+    
