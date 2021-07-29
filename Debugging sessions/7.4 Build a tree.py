c3dc1ec2e3e81fd26287cea2fc717893f4d3dbf9
Kerrie Wong <wongkerrie@gmail.com>
2021-07-28 21:23:19 -0400

    added 7.4 coding session and learned how to use the debugger

---
+++ Debugging sessions/7.4 Build a tree.py
@@ -0,0 +1,39 @@
+# https://runestone.academy/runestone/books/published/pythonds/Trees
+# section 7.4: build a tree and learn how to use the debugger in vs code
+
+def BinaryTree(r):
+    return [r, [], []]
+
+def insertLeft(root,newBranch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1,[newBranch,t,[]])
+    else:
+        root.insert(1,[newBranch, [], []])
+    return root
+
+def insertRight(root,newBranch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2,[newBranch,[],t])
+    else:
+        root.insert(2,[newBranch,[],[]])
+    return root
+
+def getRootVal(root):
+    return root[0]
+
+def setRootVal(root,newVal):
+    root[0] = newVal
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChild(root):
+    return root[2]
+
+x = BinaryTree('a')
+insertLeft(x,'b')
+insertRight(x,'c')
+insertRight(getRightChild(x),'d')
+insertLeft(getRightChild(getRightChild(x)),'e')
