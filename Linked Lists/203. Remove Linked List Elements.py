5ecf5bb89eb0110c2a9fdae8d71c99b897496c24
Kerrie Wong <wongkerrie@gmail.com>
2021-07-30 23:01:40 -0400

    updated

--- Linked Lists/203. Remove Linked List Elements.py
+++ Linked Lists/203. Remove Linked List Elements.py
@@ -45,7 +45,11 @@ class Solution:
                 
         return dummy.next
 
-# Q: What happen to when the last node matches key? In that case, node.next is none and will bypass the while loop
+# Q: What happen to when the last node matches key?
+# In that case, node.next is none and will bypass the while loop
+
+# Answer: This method looks at .next and not curr, so when last node matches key, curr.next would have 
+# already caught it and curr.next would have already been updated.
 
 # Explain:
 # Before writing any code, it's good to make a list of edge cases that we need to consider. This is so that we can be certain that we're not overlooking anything while coming up with our algorithm, and that we're testing all special cases when we're ready to test. These are the edge cases that I came up with.
