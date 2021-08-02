4833292051c957f3cd8a51c8121ed845a1750f2c
Kerrie Wong <wongkerrie@gmail.com>
2021-08-02 18:10:38 -0400

    updated

--- Dicts/1002. Find Common Characters.py
+++ Dicts/1002. Find Common Characters.py
@@ -24,3 +24,29 @@
 #     1 <= words[i].length <= 100
 #     words[i] consists of lowercase English letters.
 
+from collections import Counter 
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        # counts = set{}
+#         Above method doesn't work bc of the collections method. Fastest and most efficient way is to 
+        counts = Counter(words[0])
+        
+        for index in range(1,len(words)):
+#             x &= y is the same as x = x & y aka 'union set'
+            counts &= Counter(words[index])
+      
+    #  counts in here is the 'collection' function: Counter({'l': 2, 'e': 1})
+    #  eventhoug it is the right answer, it's not displaying the right way... we need the following loop to change the display format
+        print(counts)
+        result = []
+    #   .items() will return the result as tuples
+        for char, count in counts.items():
+            result += [char]* count
+        return result
+    
+# Pesudo:
+# iterate over the array,
+# use the build-in function: counter
+# put everything in the counter function
+# make sure the final display is in the right format
+# return the answer
