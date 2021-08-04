fffc4339a13f6aecb621dec188a31440701aefc1
Kerrie Wong <wongkerrie@gmail.com>
2021-08-04 19:28:17 -0400

    init

---
+++ Math/58. Length of Last Word.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/length-of-last-word/
+
+# Given a string s consists of some words separated by some number of spaces, return the length of the last word in the string.
+
+# A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+# Example 1:
+
+# Input: s = "Hello World"
+# Output: 5
+
+# Example 2:
+
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+
+# Example 3:
+
+# Input: s = "luffy is still joyboy"
+# Output: 6
