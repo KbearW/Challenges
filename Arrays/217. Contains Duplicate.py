9827e456a3ce7236126b8b1d5880fd3ceba4572c
Kerrie Wong <wongkerrie@gmail.com>
2021-08-05 22:45:28 -0400

    update

--- Arrays/217. Contains Duplicate.py
+++ Arrays/217. Contains Duplicate.py
@@ -19,3 +19,16 @@
 # Input: nums = [1,1,1,3,3,4,3,2,4,2]
 # Output: true
 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        counter = collections.Counter(nums)
+        # print(counter.values())
+        for num, count in counter.items():
+            # print(num, count)
+            if count >= 2:
+                return True
+        return False
+    
+# Pseudo Code:
+#     use collections.Counter built in function to put all items in a dicts
+#     then iterate over the dicts.items() and only take what's needed
