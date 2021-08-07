715d04ec6b56b43bfe0ec4dfcc4b8fdb4679f7fe
Kerrie Wong <wongkerrie@gmail.com>
2021-08-06 23:00:20 -0400

    update

--- Arrays/283. Move Zeroes.py
+++ Arrays/283. Move Zeroes.py
@@ -15,3 +15,30 @@
 
 # Input: nums = [0]
 # Output: [0]
+
+# TBD... This is complicated with an inplace solution--> O(1) space.
+# What needs to happen-->
+# use two pointer method to keep track of curr and zeroes (aka prev),
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        prev = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                hold = nums[prev]
+                nums[prev] = nums[i]
+                nums[i] = hold
+                prev += 1
+        
+# Note:
+#     two pointers, swap method
+
+# Pseudo code:
+    # iterate over the input list:
+#     if num equ 0:
+        # pop that elem and add that to the end of the list <-- This logic is off, don't pop it, swap it!
+    # (By using pop, order of the list will be retain but indexing will be off!) <-- this is wrong
+    # return list
