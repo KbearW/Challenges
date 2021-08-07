bcc67a63606558d37ef3c5dc262b7bee712836a9
Kerrie Wong <wongkerrie@gmail.com>
2021-08-07 12:27:39 -0400

    updated

--- Math/412.  Fizz Buzz.py
+++ Math/412.  Fizz Buzz.py
@@ -23,3 +23,18 @@
 
 # Input: n = 15
 # Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = []
+        for x in range(1,n+1):
+            if x % 15 == 0:
+                answer += ['FizzBuzz']
+            elif x % 5 == 0:
+                answer += ['Buzz']
+            elif x % 3 ==0:
+                answer += ['Fizz']
+            else:
+                answer += [str(x)]
+                
+        return answer
