3ca5272343209622c84932472cc4c7a6d13175bb
Kerrie Wong <wongkerrie@gmail.com>
2021-08-09 11:57:36 -0400

    init

---
+++ Debugging sessions/26. debug.py
@@ -0,0 +1,16 @@
+def removeDuplicates(nums):
+    if len(nums) == 0:
+        return 0
+    
+    prev = 0
+
+    for i in range(1,len(nums)):
+        if nums[i] != nums[prev]:  #nums[1] != nums[0]
+            prev += 1  #nums[1]
+            nums[prev] = nums[i]  #reassign: nums[1] = nums[1]
+    print(len(nums))
+    print(nums)
+    print(prev +1 )
+    return prev+1
+
+removeDuplicates([1,1,2,2,3,4,4])
