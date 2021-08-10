38f96196a8ee83a61d5a9d8dd632f2c4ff2723be
Kerrie Wong <wongkerrie@gmail.com>
2021-08-09 21:27:07 -0400

    update

--- Math/125. Valid Palindrome.py
+++ Math/125. Valid Palindrome.py
@@ -15,3 +15,30 @@
 # Input: s = "race a car"
 # Output: false
 # Explanation: "raceacar" is not a palindrome.
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # two pointers: left and right
+        # Method #1 O(n) runtime | O(1) space
+        res = ''
+        for i in s:
+            if i.isalnum():
+                res += i.lower()        
+        
+        n = len(res)    
+        left = 0
+        right = n-1  
+
+        while left < right:
+            if res[left] != res[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+    
+
+# Note:--> return T/F if doesn't match
+#     Ignore space and ,.!? + cases--> .lower() and isalnum
+#     left and right pointers,
+#     find the function for is alphabet/number--> .isalnum
