72ae2b44376d90f1d4f75333b39af0451f4484b1
Kerrie Wong <wongkerrie@gmail.com>
2021-08-12 18:26:25 -0400

    update

---
+++ Debugging sessions/189.debug.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # method #1
+        # in case k > nums
+        k = k % len(nums)
+        
+        def rev(start,end):
+            while start<end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+                
+        nums.reverse()
+        rev(0, k-1)
+        rev(k, len(nums)-1)
+    
+        
+# Steps:
+# reverse the whole array
+# reverse the k element
+# reverse the len(nums)-k elements (remainder elements)
+        
+        # memory: O(1)
