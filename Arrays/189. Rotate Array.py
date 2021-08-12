ece1093b51cb69613ae321b7f4d3e10bf1af1854
Kerrie Wong <wongkerrie@gmail.com>
2021-08-12 18:25:43 -0400

    updated

--- Arrays/189. Rotate Array.py
+++ Arrays/189. Rotate Array.py
@@ -36,3 +36,29 @@
 #     Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
 #     Could you do it in-place with O(1) extra space?
 
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # method #1
+        # in case k > nums
+        k = k % len(nums)
+        
+        def rev(start,end):
+            while start<end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+                
+        nums.reverse()
+        rev(0, k-1)
+        rev(k, len(nums)-1)
+    
+        
+# Steps:
+# reverse the whole array
+# reverse the k element
+# reverse the len(nums)-k elements (remainder elements)
+        
+        # memory: O(1)
