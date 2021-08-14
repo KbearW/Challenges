91f2ca4736407c177228c83a7501031c74dee1af
Kerrie Wong <wongkerrie@gmail.com>
2021-08-14 18:54:57 -0400

    update

--- Arrays/48. Rotate Image.py
+++ Arrays/48. Rotate Image.py
@@ -15,3 +15,21 @@
 
 # Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
 # Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+    def rotate(self, A: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # Method 1
+        # The most pythonic solution is a simple one-liner using [::-1] to flip the matrix upside down and then zip to transpose it. It assigns the result back into A, so it's "in-place" in a sense and the OJ accepts it as such, though some people might not.
+        
+        # A[:] = zip(*A[::-1])
+        # return A
+        
+        # Method 2
+        A.reverse()
+        print(A)
+        for i in range(len(A)):
+            for j in range(i):
+                A[i][j], A[j][i] = A[j][i], A[i][j]
+        return A
