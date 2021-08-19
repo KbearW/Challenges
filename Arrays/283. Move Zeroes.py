6a4dbbc081bcc0b934af9d42fd98cff3b593abb0
Kerrie Wong <wongkerrie@gmail.com>
2021-08-18 22:57:22 -0400

    update

--- Arrays/283. Move Zeroes.py
+++ Arrays/283. Move Zeroes.py
@@ -32,6 +32,13 @@ class Solution:
                 nums[prev] = nums[i]
                 nums[i] = hold
                 prev += 1
+
+#     Order:
+            # [0,1,0,3,12]
+            # [1,0,0,3,12]
+            # [1,3,0,0,12]
+            # [1,3,12,0,0]
+    
         
 # Note:
 #     two pointers, swap method
--- Debugging sessions/283. debug.py
+++ Debugging sessions/283. debug.py
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/move-zeroes/
+# https://leetcode.com/problems/move-zeroes/
 
 def moveZeroes(nums):
     """
