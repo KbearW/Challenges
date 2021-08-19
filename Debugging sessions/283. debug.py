553a6f85646f6c6341c5f6813caa30a6b001c234
Kerrie Wong <wongkerrie@gmail.com>
2021-08-18 22:19:20 -0400

    update

---
+++ Debugging sessions/283. debug.py
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/move-zeroes/
+
+def moveZeroes(nums):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    prev = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            hold = nums[prev]
+            nums[prev] = nums[i]
+            nums[i] = hold
+            prev += 1
+    print(nums)
+
+moveZeroes([0,1,0,3,12])
