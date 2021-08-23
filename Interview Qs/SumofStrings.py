0b1148e4cb34351f3fea9289e1baa6983ea08a18
Kerrie Wong <wongkerrie@gmail.com>
2021-08-22 22:29:03 -0400

    update

--- Interview Qs/SumofStrings.py
+++ Interview Qs/SumofStrings.py
@@ -1,11 +1,13 @@
 def sumOfStrings(a,b):
     sum = ''
     digits = [0] * max(len(a), len(b))
-    print(digits)
+    # print(digits)
 
+    # Didn't ask for inplace, okay to use [::-1] function here
     a = a[::-1]
     b = b[::-1]
 
+    # Can use a helper function for this
     for i in range(len(a)):
         print(i, a[i])
         digits[i] += int(a[i])
@@ -16,6 +18,7 @@ def sumOfStrings(a,b):
         digits[j] += int(b[j])
     print(digits)
 
+    # can be optiumize w .reduce() or .map() or lambda combination... look into it
     for digit in digits:
         sum = str(digit) + sum
 
