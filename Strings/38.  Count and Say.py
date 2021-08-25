f685aae8485728c9c7c6c65160316142242048de
Kerrie Wong <wongkerrie@gmail.com>
2021-08-25 17:53:48 -0400

    update

--- Strings/38.  Count and Say.py
+++ Strings/38.  Count and Say.py
@@ -35,3 +35,21 @@
 
 #     1 <= n <= 30
 
+class Solution:
+#     wrapper function to loop thu n
+    def countAndSay(self, n: int) -> str:
+        seq = '1'
+        for i in range(n-1):
+            seq = self.getnext(seq)
+        return seq
+    
+    def getnext(self,seq):
+        i, next_seq = 0, ''
+        while i<len(seq):
+            count = 1
+            while i< len(seq)-1 and seq[i] == seq[i+1]:
+                count += 1
+                i += 1
+            next_seq += str(count) + seq[i]
+            i += 1
+        return next_seq
