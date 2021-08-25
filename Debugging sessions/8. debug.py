0ed1cf09132c260e9643f32ca242d77a680b68e5
Kerrie Wong <wongkerrie@gmail.com>
2021-08-25 11:58:46 -0400

    init

---
+++ Debugging sessions/8. debug.py
@@ -0,0 +1,32 @@
+def myAtoi(str):
+
+    s = s.strip()
+    if not s:
+        return 0
+    
+    sign = 1
+    res = 0
+    i = 0
+    
+    if s[0] == '-':
+        sign = -1
+    
+    if s[0] in "-," s[0].isnumeric():
+        return 0
+    
+    for i in range(len(s)):
+        if s[i].isnumeric() == True:
+            res = res*10 + int(s[i])
+            
+    if -2*31 <sign*res < 2**31-1:
+        return sign*res
+    else:
+        return 2**31*sign
+    
+    return sign * res
+
+myAtoi("42")
+myAtoi("   -42")
+myAtoi("4193 with words")
+myAtoi("words and 987")
+myAtoi("-91283472332")
