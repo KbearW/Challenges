bcc26dcc8119f5c50d558e7661eba0e249f5fd2f
Kerrie Wong <wongkerrie@gmail.com>
2021-08-26 14:31:52 -0400

    init

--- Strings/171. Excel Sheet Column Number.py
+++ Strings/171. Excel Sheet Column Number.py
@@ -12,3 +12,11 @@
 # AA -> 27
 # AB -> 28 
 # ...
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        res = 0
+        for char in columnTitle:
+            x = ord(char)-ord('A')+1
+            res = res*26+x
+        return res
