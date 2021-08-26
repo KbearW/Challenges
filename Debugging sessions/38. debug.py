5051abcdc92c7306a8a99e5c3758568b664f74df
Kerrie Wong <wongkerrie@gmail.com>
2021-08-26 12:15:46 -0400

    update

--- Debugging sessions/38. debug.py
+++ Debugging sessions/38. debug.py
@@ -1,18 +1,35 @@
-def countAndSay(n):
-    seq = '1'
-    for i in range(n-1):
-        seq = getnext(seq)
-    return seq
 
-def getnext(seq):
-    i, next_seq = 0, ''
-    while i<len(seq):
-        count = 1
-        while i< len(seq)-1 and seq[i] == seq[i+1]:
-            count += 1
-            i += 1
-        next_seq += str(count) + seq[i]
-        i += 1
-    return next_seq
-
+def countAndSay( n):
+#         res = '1'
+#         for i in range(n-1):
+#             res = self.getnext(res)
+#         return res
     
+#     def getnext(self,res):
+#         i, next_seq = 0, ''
+#         while i<len(res):
+#             count = 1
+# # i< len(seq)-1--> limit the # of times this is being run..remember 0 indexing? 
+#             while i< len(res)-1 and res[i] == res[i+1]:
+#                 count += 1
+#                 i += 1
+# #Exit the while loop when two # ain't the same, nest the res once before moving back into the while loop
+#             next_seq += str(count) + res[i]
+#             i += 1
+#         return next_seq
+
+        s = '1'
+        for i in range(n-1):
+            count = 1
+            temp = []
+            for index in range(1, len(s)):
+                if s[index] == s[index-1]:
+                    count += 1
+                else:
+                    temp.append(str(count))
+                    temp.append(s[index-1])
+                    count = 1
+            temp.append(str(count))
+            temp.append(s[-1])
+            s = ''.join(temp)
+        return s
