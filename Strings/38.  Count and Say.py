2357f75fcc20dbf91b34cc4079f3e0b96db89c25
Kerrie Wong <wongkerrie@gmail.com>
2021-08-26 12:16:02 -0400

    update

--- Strings/38.  Count and Say.py
+++ Strings/38.  Count and Say.py
@@ -34,22 +34,38 @@
 # Constraints:
 
 #     1 <= n <= 30
-
 class Solution:
-#     wrapper function to loop thu n
     def countAndSay(self, n: int) -> str:
-        seq = '1'
-        for i in range(n-1):
-            seq = self.getnext(seq)
-        return seq
+#         res = '1'
+#         for i in range(n-1):
+#             res = self.getnext(res)
+#         return res
     
-    def getnext(self,seq):
-        i, next_seq = 0, ''
-        while i<len(seq):
+#     def getnext(self,res):
+#         i, next_seq = 0, ''
+#         while i<len(res):
+#             count = 1
+# # i< len(seq)-1--> limit the # of times this is being run..remember 0 indexing? 
+#             while i< len(res)-1 and res[i] == res[i+1]:
+#                 count += 1
+#                 i += 1
+# #Exit the while loop when two # ain't the same, nest the res once before moving back into the while loop
+#             next_seq += str(count) + res[i]
+#             i += 1
+#         return next_seq
+
+        s = '1'
+        for i in range(n-1):
             count = 1
-            while i< len(seq)-1 and seq[i] == seq[i+1]:
-                count += 1
-                i += 1
-            next_seq += str(count) + seq[i]
-            i += 1
-        return next_seq
+            temp = []
+            for index in range(1, len(s)):
+                if s[index] == s[index-1]:
+                    count += 1
+                else:
+                    temp.append(str(count))
+                    temp.append(s[index-1])
+                    count = 1
+            temp.append(str(count))
+            temp.append(s[-1])
+            s = ''.join(temp)
+        return s
