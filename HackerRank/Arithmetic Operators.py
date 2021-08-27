3ca633476f51aab690f148b1209fbaa927fa230b
Kerrie Wong <wongkerrie@gmail.com>
2021-08-27 11:06:50 -0400

    update

--- HackerRank/Arithmetic Operators.py
+++ HackerRank/Arithmetic Operators.py
@@ -1,25 +1,25 @@
-https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
 
-Task
-The provided code stub reads two integers from STDIN, and
+# Task
+# The provided code stub reads two integers from STDIN, and
 
-. Add code to print three lines where:
+# . Add code to print three lines where:
 
-    The first line contains the sum of the two numbers.
-    The second line contains the difference of the two numbers (first - second).
-    The third line contains the product of the two numbers.
+#     The first line contains the sum of the two numbers.
+#     The second line contains the difference of the two numbers (first - second).
+#     The third line contains the product of the two numbers.
 
-Example
+# Example
 
-Print the following:
+# Print the following:
 
-8
--2
-15
+# 8
+# -2
+# 15
 
-Input Format
+# Input Format
 
-The first line contains the first integer,
-.
-The second line contains the second integer, . 
+# The first line contains the first integer,
+# .
+# The second line contains the second integer, . 
 
