85cd920641be37b4235279dc1b6fd4008ae1bf56
Kerrie Wong <wongkerrie@gmail.com>
2021-08-27 11:08:08 -0400

    init

--- HackerRank/Staircase.py
+++ HackerRank/Staircase.py
@@ -1,51 +1,75 @@
-https://www.hackerrank.com/challenges/staircase/problem
+# https://www.hackerrank.com/challenges/staircase/problem
 
-Staircase detail
+# Staircase detail
 
-This is a staircase of size
+# This is a staircase of size
 
-:
+# :
 
-   #
-  ##
- ###
-####
+#    #
+#   ##
+#  ###
+# ####
 
-Its base and height are both equal to
+# Its base and height are both equal to
 
-. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+# . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
 
-Write a program that prints a staircase of size
+# Write a program that prints a staircase of size
 
-.
+# .
 
-Function Description
+# Function Description
 
-Complete the staircase function in the editor below.
+# Complete the staircase function in the editor below.
 
-staircase has the following parameter(s):
+# staircase has the following parameter(s):
 
-    int n: an integer
+#     int n: an integer
 
-Print
+# Print
 
-Print a staircase as described above.
+# Print a staircase as described above.
 
-Input Format
+# Input Format
 
-A single integer,
+# A single integer,
 
-, denoting the size of the staircase.
+# , denoting the size of the staircase.
 
-Constraints
+# Constraints
 
-.
+# .
 
-Output Format
+# Output Format
 
-Print a staircase of size
+# Print a staircase of size
 
-using # symbols and spaces.
+# using # symbols and spaces.
 
-Note: The last line must have
-spaces in it. 
+# Note: The last line must have
+# spaces in it. 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    # Write your code here
+    for num in range(1,n+1):
+        print(' '*(n-num)+'#'*num)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
