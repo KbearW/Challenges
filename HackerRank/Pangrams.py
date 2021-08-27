a52e5cf9c55121de3daa32bdd2f229b47b11e841
Kerrie Wong <wongkerrie@gmail.com>
2021-08-27 15:39:06 -0400

    init

--- HackerRank/Pangrams.py
+++ HackerRank/Pangrams.py
@@ -1,56 +1,91 @@
-https://www.hackerrank.com/challenges/pangrams/problem
+# https://www.hackerrank.com/challenges/pangrams/problem
 
-A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
+# A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
 
-Example
+# Example
 
-The string contains all letters in the English alphabet, so return pangram.
+# The string contains all letters in the English alphabet, so return pangram.
 
-Function Description
+# Function Description
 
-Complete the function pangrams in the editor below. It should return the string pangram if the input string is a pangram. Otherwise, it should return not pangram.
+# Complete the function pangrams in the editor below. It should return the string pangram if the input string is a pangram. Otherwise, it should return not pangram.
 
-pangrams has the following parameter(s):
+# pangrams has the following parameter(s):
 
-    string s: a string to test
+#     string s: a string to test
 
-Returns
+# Returns
 
-    string: either pangram or not pangram
+#     string: either pangram or not pangram
 
-Input Format
+# Input Format
 
-A single line with string
+# A single line with string
 
-.
+# .
 
-Constraints
+# Constraints
 
 
-Each character of ,
+# Each character of ,
 
-Sample Input
+# Sample Input
 
-Sample Input 0
+# Sample Input 0
 
-We promptly judged antique ivory buckles for the next prize
+# We promptly judged antique ivory buckles for the next prize
 
-Sample Output 0
+# Sample Output 0
 
-pangram
+# pangram
 
-Sample Explanation 0
+# Sample Explanation 0
 
-All of the letters of the alphabet are present in the string.
+# All of the letters of the alphabet are present in the string.
 
-Sample Input 1
+# Sample Input 1
 
-We promptly judged antique ivory buckles for the prize
+# We promptly judged antique ivory buckles for the prize
 
-Sample Output 1
+# Sample Output 1
 
-not pangram
+# not pangram
 
-Sample Explanation 0
+# Sample Explanation 0
 
-The string lacks an x. 
+# The string lacks an x. 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import collections
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    sset = set(s.lower())
+    
+    if len(sset) != 27:
+        return 'not pangram'
+    else:
+        return 'pangram'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
