3b7fa1b964a617249791d83aa4f55ab944d4753c
Kerrie Wong <wongkerrie@gmail.com>
2021-08-27 16:54:41 -0400

    update

--- HackerRank/debug- Super Reduced String.py
+++ HackerRank/debug- Super Reduced String.py
@@ -1,12 +1,22 @@
 def function(s):
-    res = ''
-    while len(s)<len(res)+1:
-        for i, char in enumerate(s):
-            if s[i] != s[i+1]:
-                res += s[i]
-                print(res)
-    if not res:
-        return 'Empty String'
-    return res
+    # res = ''
+    # while len(s)>len(res):
+    #     for i, char in enumerate(s):
+    #         if s[i] != s[i+1]:
+    #             res += s[i]
+    #             print(res)
+    # if not res:
+    #     return 'Empty String'
+    # return res
 
-function('aaabccddd')
+    stack = []
+    for char in s:
+        if stack and char == stack[-1]:
+            stack.pop()
+        else:
+            stack.append(char)
+    if not stack:
+        return 'Empty String'
+    return ''.join(stack)
+    
+print(function('aaabccddd'))
