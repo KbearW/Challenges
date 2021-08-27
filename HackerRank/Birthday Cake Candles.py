17ff7b94023dcad33f593e791ae092d94c56e085
Kerrie Wong <wongkerrie@gmail.com>
2021-08-27 11:10:21 -0400

    init

--- HackerRank/Birthday Cake Candles.py
+++ HackerRank/Birthday Cake Candles.py
@@ -30,3 +30,36 @@
 
 # Constraints
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import collections
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    count = collections.Counter(candles)
+    return max(count.values())
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
