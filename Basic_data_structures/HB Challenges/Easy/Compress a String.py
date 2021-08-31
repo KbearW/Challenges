8ad891da06593f12d7f5b2a5aedc760b8eba98f5
Kerrie Wong <wongkerrie@gmail.com>
2021-08-31 11:30:04 -0400

    init

--- Basic_data_structures/HB Challenges/Easy/Compress a String.py
+++ Basic_data_structures/HB Challenges/Easy/Compress a String.py
@@ -1,2 +1,28 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/compress-string/index.html#compress-string
 
+def compstring(words):
+    # goal: return the shortest length of words
+    res = []
+    curr = ''
+    count = 1
+    # append the 1st item to 'res'
+    curr += words[0]
+    # iterate over words   
+
+    # index error
+    for i, word in enumerate(words):
+    # if first char equals to next
+        if i < len(word):
+            if word == words[i+1]:
+                count += 1
+        # increase count
+        # else, carry over count to 'res'
+            else:
+                res += str(count)
+        # keep looping until the end of words
+        # compare the length of res and words
+    print(len(res), len(words))
+    # return (min(len(res), min(len(words))))
+    # return the shortest length of word string
+
+print(compstring('aabbaabb'))
---
+++ Basic_data_structures/HB Challenges/Easy/Fit String to Width.py
@@ -0,0 +1,17 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/fit-to-width/index.html#fit-to-width
+
+def fitstringtowidth(words, limit):
+    statement = ''
+    if words[limit] == ' ':
+        
+
+
+# how to solve it in word:
+# goal: return strings that fit to width
+# ideas:
+#     statement = ''
+#     find the limit if not equals to whitespace
+#         finding the closest white space before the limit, 
+#     print the string up till the limit
+#     reset and repeat 
+    
