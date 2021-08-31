e77dfd2f5e510037e08b49cde3ee7e99ba3e8f08
Kerrie Wong <wongkerrie@gmail.com>
2021-08-31 11:30:29 -0400

    update

--- HackerRank/sWAP cASE.py
+++ HackerRank/sWAP cASE.py
@@ -1,25 +1,52 @@
-https://www.hackerrank.com/challenges/swap-case/problem
+# https://www.hackerrank.com/challenges/swap-case/problem
 
-You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+# You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
 
-For Example:
+# For Example:
 
-Www.HackerRank.com → wWW.hACKERrANK.COM
-Pythonist 2 → pYTHONIST 2  
+# Www.HackerRank.com → wWW.hACKERrANK.COM
+# Pythonist 2 → pYTHONIST 2  
 
-Function Description
+# Function Description
 
-Complete the swap_case function in the editor below.
+# Complete the swap_case function in the editor below.
 
-swap_case has the following parameters:
+# swap_case has the following parameters:
 
-    string s: the string to modify
+#     string s: the string to modify
 
-Returns
+# Returns
 
-    string: the modified string
+#     string: the modified string
 
-Input Format
+# Input Format
 
-A single line containing a string
-.
+# A single line containing a string
+# .
+
+def swap_case(s):
+    res = ''
+    for char in s:
+        if char.isalpha() == True:
+            if char.isupper() == True:
+                res += char.lower()
+            if char.islower() == True:
+                res += char.upper()
+        else:
+            res += char
+    return res
+
+    # Steps:
+    # New var to store the modified stings
+    # iterate over the input
+        
+    # check if each char is alpha
+            # if char is upper case
+        #         append char in lower case
+        #     else: (if char is lower case)
+        #         append char in upper case
+    # else- not alpha
+        # append 
+    # return new var
+    
+if __name__ == '__main__':
