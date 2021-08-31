143197836227b1c84fc86f485ddd7bc06dfd2766
Kerrie Wong <wongkerrie@gmail.com>
2021-08-31 11:50:02 -0400

    init

---
+++ HackerRank/What's Your Name?.py
@@ -0,0 +1,31 @@
+https://www.hackerrank.com/challenges/whats-your-name/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+
+    Hello firstname lastname! You just delved into python.
+
+Function Description
+
+Complete the print_full_name function in the editor below.
+
+print_full_name has the following parameters:
+
+    string first: the first name
+    string last: the last name
+
+Prints
+
+    string: 'Hello 
+
+! You just delved into python' where and are replaced with and
+
+    .
+
+Input Format
+
+The first line contains the first name, and the second line contains the last name.
+
+Constraints
+
+The length of the first and last names are each ≤
+.
