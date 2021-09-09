ea4f7a735befd1a23486ad0b97f8e5063f321879
Kerrie Wong <wongkerrie@gmail.com>
2021-09-08 22:47:46 -0400

    update

--- HackerRank/Finding the percentage.py
+++ HackerRank/Finding the percentage.py
@@ -23,3 +23,17 @@
 
 # Print one line: The average of the marks obtained by the particular student correct to 2 decimal places.
 
+import numbers
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    student_average = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+        student_average[name] = sum(scores)/len(scores)
+
+    query_name = input()
+    print(f'{student_average[query_name]:.2f}')
+    
