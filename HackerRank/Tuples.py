5f9e26890f6e58b7ebcf06f5e1a7c4f1223ae401
Kerrie Wong <wongkerrie@gmail.com>
2021-09-08 23:01:45 -0400

    update

--- HackerRank/Tuples.py
+++ HackerRank/Tuples.py
@@ -1,32 +1,40 @@
-https://www.hackerrank.com/challenges/python-tuples/problem
+# https://www.hackerrank.com/challenges/python-tuples/problem
 
-Task
-Given an integer, , and space-separated integers as input, create a tuple, , of those integers. Then compute and print the result of
+# Task
+# Given an integer, , and space-separated integers as input, create a tuple, , of those integers. Then compute and print the result of
 
-.
+# .
 
-Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
+# Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
 
-Input Format
+# Input Format
 
-The first line contains an integer,
-, denoting the number of elements in the tuple.
-The second line contains space-separated integers describing the elements in tuple
+# The first line contains an integer,
+# , denoting the number of elements in the tuple.
+# The second line contains space-separated integers describing the elements in tuple
 
-.
+# .
 
-Output Format
+# Output Format
 
-Print the result of
+# Print the result of
 
-.
+# .
 
-Sample Input 0
+# Sample Input 0
 
-2
-1 2
+# 2
+# 1 2
 
-Sample Output 0
+# Sample Output 0
 
-3713081631934410656
+# 3713081631934410656
 
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    # print(n, integer_list)
+    list1 = [i for i in range(1, n+1)]
+    T= tuple(list1)
+    print(hash(T))
+#  (as a list is not hashable, but a tuple is)
