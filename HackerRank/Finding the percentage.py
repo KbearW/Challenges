12e44f97092a38bdb267c03f83f12879044672bb
Kerrie Wong <wongkerrie@gmail.com>
2021-09-08 22:47:27 -0400

    update

--- HackerRank/Finding the percentage.py
+++ HackerRank/Finding the percentage.py
@@ -1,24 +1,25 @@
-https://www.hackerrank.com/challenges/finding-the-percentage/problem
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem
 
-The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+# The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal.
 
-Example
+# Example
 
 
 
-The query_name is 'beta'. beta's average score is
+# The query_name is 'beta'. beta's average score is
 
-.
+# .
 
-Input Format
+# Input Format
 
-The first line contains the integer
-, the number of students' records. The next
+# The first line contains the integer
+# , the number of students' records. The next
 
-lines contain the names and marks obtained by a student, each value separated by a space. The final line contains query_name, the name of a student to query.
+# lines contain the names and marks obtained by a student, each value separated by a space. The final line contains query_name, the name of a student to query.
 
-Constraints
+# Constraints
 
-Output Format
+# Output Format
 
-Print one line: The average of the marks obtained by the particular student correct to 2 decimal places.
+# Print one line: The average of the marks obtained by the particular student correct to 2 decimal places.
+
