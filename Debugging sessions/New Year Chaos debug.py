c4654572c917b9f73f826c8f7294627aa23551c3
Kerrie Wong <wongkerrie@gmail.com>
2021-09-16 23:05:55 -0400

    init

---
+++ Debugging sessions/New Year Chaos debug.py
@@ -0,0 +1,16 @@
+# https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+def minimumBribes(q):
+    # Write your code here
+    count = 0
+    for i in range(len(q)):    
+        # print(q[i], i+1, q[i] - (i+1))
+        if q[i] - (i+1) > 2:
+            print('Too chaotic')
+            return  #this return is needed to provent keep looping over to the next print
+        if q[i] - (i+1) > 0:
+            if q[i] - (i+1) == 1 or q[i] - (i+1) == 2:  
+                count += q[i] - (i +1)
+    print(count)
+
+minimumBribes([1,2,5,3,7,8,6,4])
