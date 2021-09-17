6c5bde95b21ddc0ccb05c246fe47c8ab217e45f3
Kerrie Wong <wongkerrie@gmail.com>
2021-09-16 22:30:45 -0400

    update

--- HackerRank/New Year Chaos.py
+++ HackerRank/New Year Chaos.py
@@ -1,67 +1,67 @@
-https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+# https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 
-It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from to
+# It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from to
 
-. Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
+# . Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
 
-Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+# Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
 
-Example
+# Example
 
-If person bribes person , the queue will look like this: . Only
+# If person bribes person , the queue will look like this: . Only
 
-bribe is required. Print 1.
+# bribe is required. Print 1.
 
-Person had to bribe
+# Person had to bribe
 
-people to get to the current position. Print Too chaotic.
+# people to get to the current position. Print Too chaotic.
 
-Function Description
+# Function Description
 
-Complete the function minimumBribes in the editor below.
+# Complete the function minimumBribes in the editor below.
 
-minimumBribes has the following parameter(s):
+# minimumBribes has the following parameter(s):
 
-    int q[n]: the positions of the people after all bribes
+#     int q[n]: the positions of the people after all bribes
 
-Returns
+# Returns
 
-    No value is returned. Print the minimum number of bribes necessary or Too chaotic if someone has bribed more than 
+#     No value is returned. Print the minimum number of bribes necessary or Too chaotic if someone has bribed more than 
 
-    people.
+#     people.
 
-Input Format
+# Input Format
 
-The first line contains an integer
+# The first line contains an integer
 
-, the number of test cases.
+# , the number of test cases.
 
-Each of the next
-pairs of lines are as follows:
-- The first line contains an integer , the number of people in the queue
-- The second line has
+# Each of the next
+# pairs of lines are as follows:
+# - The first line contains an integer , the number of people in the queue
+# - The second line has
 
-space-separated integers describing the final state of the queue.
+# space-separated integers describing the final state of the queue.
 
-Constraints
+# Constraints
 
-Subtasks
+# Subtasks
 
-For
-score
-For score
+# For
+# score
+# For score
 
-Sample Input
+# Sample Input
 
-STDIN       Function
------       --------
-2           t = 2
-5           n = 5
-2 1 5 3 4   q = [2, 1, 5, 3, 4]
-5           n = 5
-2 5 1 3 4   q = [2, 5, 1, 3, 4]
+# STDIN       Function
+# -----       --------
+# 2           t = 2
+# 5           n = 5
+# 2 1 5 3 4   q = [2, 1, 5, 3, 4]
+# 5           n = 5
+# 2 5 1 3 4   q = [2, 5, 1, 3, 4]
 
-Sample Output
+# Sample Output
 
-3
-Too chaotic
+# 3
+# Too chaotic
