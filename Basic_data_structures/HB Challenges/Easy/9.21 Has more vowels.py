20b8f727283135c781641cffb9d070c1db7248a5
Kerrie Wong <wongkerrie@gmail.com>
2021-09-21 17:12:23 -0400

    init

--- Basic_data_structures/HB Challenges/Easy/9.21 Has more vowels.py
+++ Basic_data_structures/HB Challenges/Easy/9.21 Has more vowels.py
@@ -1,2 +1,30 @@
-https://fellowship.hackbrightacademy.com/materials/challenges/has-more-vowels/index.html#has-more-vowels
+# https://fellowship.hackbrightacademy.com/materials/challenges/has-more-vowels/index.html#has-more-vowels
 
+# Goal: return true if contains more vowels than non-vowels, else return False
+# will have upper and lower cases
+
+# def function
+# vowels = 'aeiou'
+# count = 0
+# iterate over input
+# if in vowels, count +1
+# if len(input) - count > count:
+# return False
+# else return True
+
+def has_more_vowels(input):
+    '''answer the Q: contains more vowels han non-vowels?'''
+    # can use set here...> {'a','e','i','o','u'}
+    vowels = 'aeiou'
+    count = 0
+    for char in input.lower():
+        if char in vowels:
+            count += 1
+    if len(input) - count >= count:
+        return False
+    else:
+        return True
+
+
+input = 'yay'
+print(has_more_vowels(input))
---
+++ Basic_data_structures/HB Challenges/Easy/9.21 Has unique char.py
@@ -0,0 +1,2 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/has-unique-chars/index.html#has-unique-chars
+
