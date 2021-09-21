0bdc0dbe8f75a9acf1c8872afec12e7326485ea3
Kerrie Wong <wongkerrie@gmail.com>
2021-09-20 22:45:31 -0400

    update

--- Basic_data_structures/HB Challenges/Easy/9.20 Add to zero.py
+++ Basic_data_structures/HB Challenges/Easy/9.20 Add to zero.py
@@ -1,2 +1,22 @@
-https://fellowship.hackbrightacademy.com/materials/challenges/add-to-zero/index.html#add-to-zero
+# https://fellowship.hackbrightacademy.com/materials/challenges/add-to-zero/index.html#add-to-zero
 
+
+# Goal: return True if any two nums in list sum to 0.and
+
+# def a function
+# condition: 
+# if 0 in list:
+#     True
+# iterate over the list and see if the -num in the list:
+#     return True 
+
+def add_to_zero(nums):
+    '''given list of ints, return True if any two nums sum to 0.'''
+    nums = set(nums)  #<-- by using set(), run time will be O(1)
+    for num in nums:
+        if -num in nums:
+            return True
+    return False
+
+nums = [0,1,2]
+print(add_to_zero(nums))
