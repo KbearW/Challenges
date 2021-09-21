a928f432df64d7b3078ddbbd57e5412b5fa75b07
Kerrie Wong <wongkerrie@gmail.com>
2021-09-21 17:19:07 -0400

    update

--- Basic_data_structures/HB Challenges/Easy/9.21 Has unique char.py
+++ Basic_data_structures/HB Challenges/Easy/9.21 Has unique char.py
@@ -1,2 +1,21 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/has-unique-chars/index.html#has-unique-chars
 
+# Goal: if contains only unique char, return True, else False. 
+# edge: case senstitive, if the char is empty, return True
+
+def has_unique_chars(word):
+    # Method #1
+    # char_dict = {}
+    # for char in word:
+    #     if char in char_dict.keys():
+    #         return False
+    #     else:
+    #         char_dict[char] = 1
+    # return True
+    
+    # Method #2
+    unique_char = set(word)
+    return len(word) == len(unique_char)
+
+word = 'Bob'
+print(has_unique_chars(word))
