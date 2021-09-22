bf563b25044f67f24b678a58e9243c73e3143005
Kerrie Wong <wongkerrie@gmail.com>
2021-09-22 16:43:19 -0400

    update

--- Basic_data_structures/HB Challenges/Easy/9.22 Longest word.py
+++ Basic_data_structures/HB Challenges/Easy/9.22 Longest word.py
@@ -1,1 +1,25 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/longest-word/index.html#longest-word
+
+def find_longest_word(words):
+    '''takes a list of words and returns the length of the longest one'''
+    # Method #1- use build-in method- max
+    # len_of_word = []
+    # for word in words:
+    #     len_of_word.append(len(word))
+    # return max(len_of_word)
+    # Runtime: O(n)
+    # Memory: O(n)
+
+    # Method #2- use O(1) memory
+    longest_len = len(words[0])
+    for word in words:
+        if len(word) > longest_len:
+            longest_len = len(word)
+    return longest_len
+
+    # runtime: O(n)
+    # Memory: O(1)
+
+word_list = [["hi", "hello"],["Balloonicorn", "Hackbright"]]
+for word_pair in word_list:
+    print(find_longest_word(word_pair))
