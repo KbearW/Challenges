f4ad517d1a4f01ed3f4b2bb0843bfed83fb6fa9c
Kerrie Wong <wongkerrie@gmail.com>
2021-09-22 18:19:01 -0400

    update

--- Basic_data_structures/HB Challenges/Easy/9.22 Pangram.py
+++ Basic_data_structures/HB Challenges/Easy/9.22 Pangram.py
@@ -1,1 +1,14 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/pangram/index.html#pangram
+
+'''return T/F if a sentence contains all the letters of english alphabet at least once.'''
+# edge case: not case senstitive -->.lower()
+# what about non alpha? --> use .isalpha()
+# use set to remove duplicates
+
+def is_pangram(sentence):
+
+    return len(set(char.lower() for char in sentence if char.isalpha()))==26
+
+sentences = ["The quick brown fox jumps over the lazy dog!", "I like cats, but not mice"]
+for sentence in sentences:
+    print(is_pangram(sentence))
