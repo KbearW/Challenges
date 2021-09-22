631061519378250a5fe1539b307b25a8c3e48dd5
Kerrie Wong <wongkerrie@gmail.com>
2021-09-21 21:06:05 -0400

    init

--- Basic_data_structures/HB Challenges/Easy/9.21 Is Palindrome.py
+++ Basic_data_structures/HB Challenges/Easy/9.21 Is Palindrome.py
@@ -1,2 +1,15 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/is-palindrome/index.html#is-palindrome
 
+# goal: retrun T/F if word is a palindorme--> spelled the same backwards and forwards
+# edge: case senstitive
+
+def is_palindorme(word):
+    '''T/F if word is a palindorme'''
+    for i in range(len(word)//2):
+        if word[i] != word[-i-1]:
+            return False
+    return True
+
+words = ['a', 'noon', 'racecar', 'porcupine', 'Racecar']
+for word in words:
+    print(f'for {word}, palindorme is {is_palindorme(word)}.')
---
+++ Basic_data_structures/HB Challenges/Easy/9.21 Largest smaller than.py
@@ -0,0 +1,2 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/largest-smaller-than/index.html#largest-smaller-than
+
