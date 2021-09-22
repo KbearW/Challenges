826894379e352d75490e07d3cb225918a7736b1d
Kerrie Wong <wongkerrie@gmail.com>
2021-09-21 20:41:11 -0400

    update

--- Basic_data_structures/HB Challenges/Easy/9.21 Is number prime.py
+++ Basic_data_structures/HB Challenges/Easy/9.21 Is number prime.py
@@ -2,16 +2,35 @@
 
 # goal: if prime--> True, else--> False
 # edge: if less than or equ to 1 --> False
+import math
 
 def is_prime(num):
-    # primes = [2,3,5,7,11,13,17]
+    '''if num is prime, return True, else False'''
     if num <= 1:
         return False
-    for n in range(2,num):
+
+    # Method #1
+    # for n in range(2,num):
+    #     if num % n == 0:
+    #         return False
+    # return True
+
+    # Method #2
+    '''this method is faster bc the begining of the factorization and ending of it will be div by the sqrt
+    of the num, therefore, the run time of this is log(n) w base 2.'''
+    # max_divisor = math.floor(math.sqrt(num))
+    # for n in range(2, max_divisor + 1):
+    #     if num % n == 0:
+    #         return False
+    # return True
+
+    # Method #3
+    '''this method will elimate all the even num that's greater than 2 to cutdown on runtime--> log(n/2)'''
+    max_divisor = math.floor(math.sqrt(num))
+    for n in range(2, max_divisor + 1, 2):  # the last elem of the range function is 'step', how many # to skip over
         if num % n == 0:
             return False
     return True
 
-
-num = 11
+num = 4
 print(is_prime(num))
