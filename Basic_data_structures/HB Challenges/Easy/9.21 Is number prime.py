a3d5886456d18e050bcd325b7455c21b4f946fa4
Kerrie Wong <wongkerrie@gmail.com>
2021-09-21 20:28:10 -0400

    update

--- Basic_data_structures/HB Challenges/Easy/9.21 Is number prime.py
+++ Basic_data_structures/HB Challenges/Easy/9.21 Is number prime.py
@@ -1,3 +1,17 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/is-prime/index.html#is-prime
 
-goal: if prime--> True, else--> False
+# goal: if prime--> True, else--> False
+# edge: if less than or equ to 1 --> False
+
+def is_prime(num):
+    # primes = [2,3,5,7,11,13,17]
+    if num <= 1:
+        return False
+    for n in range(2,num):
+        if num % n == 0:
+            return False
+    return True
+
+
+num = 11
+print(is_prime(num))
