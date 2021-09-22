ae953366180e061aa510504e085f9bc5d3410002
Kerrie Wong <wongkerrie@gmail.com>
2021-09-22 17:56:47 -0400

    init

--- Basic_data_structures/HB Challenges/Easy/9.22 Max of Three.py
+++ Basic_data_structures/HB Challenges/Easy/9.22 Max of Three.py
@@ -1,2 +1,32 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/max-of-three/index.html#max-of-three
 
+def max_of_three(num1, num2, num3):
+    '''takes in 3 nums as arg and returns the largest num'''
+    # Method #1
+
+    if num1 >= num2 and num1 >= num3:
+        return num1
+
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+
+    elif num3 >= num1 and num3 >= num2:
+        return num3
+
+    else:
+        return 'error'
+    
+
+    # Method #2
+    # numlist = [num1, num2, num3]
+    # return max(numlist)
+
+# edge cases: neg? 0? Not empty, whole num
+# put in a list and sorted()
+# runtime: O(n)
+# space: O(1)
+
+num1 = 1
+num2 = 5
+num3 = 2
+print(max_of_three(num1, num2, num3))
