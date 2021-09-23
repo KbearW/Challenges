bb860b76f329ab182528d2044f4bb1b9760cb262
Kerrie Wong <wongkerrie@gmail.com>
2021-09-23 09:43:03 -0400

    update

--- Basic_data_structures/HB Challenges/Easy/9.23 Reverse a list in place.py
+++ Basic_data_structures/HB Challenges/Easy/9.23 Reverse a list in place.py
@@ -1,1 +1,16 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/rev-list-in-place/index.html#rev-list-in-place
+
+'''goal: reverse a list in place and uses O(1) Memory only, runtime: O(n/2)'''
+
+# swap order, try not to use the built in method
+
+def rev_list_in_place(lst):
+    '''reverse list in place, don't use reversed(), .reverse() or list slice assignment'''
+    for i in range(int(len(lst) // 2)):
+        # print(i+1, -(i+1))
+        lst[i], lst[-(i+1)] = lst[-(i+1)], lst[i]
+    return lst
+
+lsts = [[1,2,3], [3,4,6,7], [1,2,3,4,5,6,7,8,9]]
+for lst in lsts:
+    print(rev_list_in_place(lst))
