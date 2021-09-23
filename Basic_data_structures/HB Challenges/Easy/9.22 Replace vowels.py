9e6f3eebb7210a12e094c748137704a6abe16616
Kerrie Wong <wongkerrie@gmail.com>
2021-09-23 09:27:23 -0400

    update

--- Basic_data_structures/HB Challenges/Easy/9.22 Replace vowels.py
+++ Basic_data_structures/HB Challenges/Easy/9.22 Replace vowels.py
@@ -1,2 +1,23 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/replace-vowels/index.html#replace-vowels
 
+'''given list of chars, return a new cpy, but w vowels replaced by *'''
+# edge case: empty list --> empty list
+# case none senstitive
+# vowels= 'aeiou'
+
+def replace_vowels(chars):
+    '''given list of chars, return a new cpy, but w vowels replaced by *'''
+    # Method #1
+    # res = []
+    # for char in chars:
+    #     if char.lower() in 'aeiou':
+    #         char = '*'
+    #     res.append(char)
+    # return res
+
+    # Method #2
+    return ['*' if char.lower() in 'aeiou' else char for char in chars]
+
+chars_list = [['h', 'i'],['o', 'o', 'o'], ['z', 'z', 'z'],['A','b','C'] ,[], ['y','a','y']]
+for chars in chars_list:
+    print(replace_vowels(chars))
