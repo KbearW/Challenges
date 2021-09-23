116bdd905a71bf2bcb2356c40d73bf468f059b82
Kerrie Wong <wongkerrie@gmail.com>
2021-09-23 09:49:00 -0400

    update

--- Basic_data_structures/HB Challenges/Easy/9.23 Reverse a String.py
+++ Basic_data_structures/HB Challenges/Easy/9.23 Reverse a String.py
@@ -1,2 +1,16 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/rev-string/index.html#rev-string
 
+'''reverse the string given. cannot use reverse or reversed()'''
+# case not senstitive
+
+# iterate over the string and use slice method to rebuild a word, runtime: O(n**2), space: O(n)
+
+def rev_string(word):
+    res = ''
+    for i in range(len(word)+1):
+        res += word[-(i+1)]
+    return res
+
+words = ['porcupine','Abc',[]]
+for word in words:
+    print(rev_string(word))
