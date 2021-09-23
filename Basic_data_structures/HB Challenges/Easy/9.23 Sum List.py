2e3803fb480d6c83d2c9d65d7689f008b6ebf221
Kerrie Wong <wongkerrie@gmail.com>
2021-09-23 10:36:03 -0400

    update

--- Basic_data_structures/HB Challenges/Easy/9.23 Sum List.py
+++ Basic_data_structures/HB Challenges/Easy/9.23 Sum List.py
@@ -1,1 +1,16 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/sum-list/index.html#sum-list
+
+# goal: sum the list of num
+def sum_list(num_list):
+
+    # Method #1- builtin method
+    # return sum(num_list)
+
+    # Method #2
+    total = 0
+    for num in num_list:
+        total += num
+    return total
+
+num_list = [5,3,6,2,1]
+print(sum_list(num_list))
