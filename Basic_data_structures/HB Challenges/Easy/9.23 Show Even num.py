c20bcae1be90bb3fba5c50765a7cd503561647a6
Kerrie Wong <wongkerrie@gmail.com>
2021-09-23 10:28:26 -0400

    update

--- Basic_data_structures/HB Challenges/Easy/9.23 Show Even num.py
+++ Basic_data_structures/HB Challenges/Easy/9.23 Show Even num.py
@@ -1,2 +1,16 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/show-evens/index.html#show-evens
 
+# edge: neg--> only pos whole num
+# runtime: O(n)
+# space: O(1)
+
+def show_evens(nums):
+    '''return a list of the index of the evens'''
+    res = []
+    for i, num in enumerate(nums):
+        if num % 2 == 0:
+            res.append(i)
+    return res
+
+nums = [1,2,3,4,6,8]
+print(show_evens(nums))
