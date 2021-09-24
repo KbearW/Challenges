1cd4bddc803869d966c7fb578e001f69830ef034
Kerrie Wong <wongkerrie@gmail.com>
2021-09-24 11:33:22 -0400

    init

--- Basic_data_structures/HB Challenges/Easy/9.23 Truncate repeats.py
+++ Basic_data_structures/HB Challenges/Easy/9.23 Truncate repeats.py
@@ -1,2 +1,15 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/truncate-repeats/index.html#truncate-repeats
 
+'''input: string, output: string '''
+
+def truncate(word):
+    res = []
+    
+    for char in word:
+        if not res or char != res[-1]:
+            res.append(char)
+
+    return ''.join(res)
+
+word = 'aaaaabbbbcccaaaa'
+print(truncate(word))
---
+++ Basic_data_structures/HB Challenges/Easy/9.24 Word count.py
@@ -0,0 +1,2 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/word-count/index.html#word-count
+
