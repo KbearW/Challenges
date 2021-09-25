aad605a3e14462c3c63523bf754e467e13d43efc
Kerrie Wong <wongkerrie@gmail.com>
2021-09-24 22:42:04 -0400

    update

--- Basic_data_structures/HB Challenges/Medium/9.24 Count List Recursively.py
+++ Basic_data_structures/HB Challenges/Medium/9.24 Count List Recursively.py
@@ -1,2 +1,14 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/count-recursively/index.html#count-recursively
 
+def count_recursively(arr):
+    '''count # of items in a list using recursion'''
+    
+    # base case:
+    if not arr:
+        return 0
+    
+    # else:
+    return count_recursively(arr[1:]) +1
+    
+
+print(count_recursively([4,2,6,8,3]))
