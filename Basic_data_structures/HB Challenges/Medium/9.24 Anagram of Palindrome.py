268ac25ccadd6b6128ba661c31c20a8d05a329de
Kerrie Wong <wongkerrie@gmail.com>
2021-09-24 21:22:42 -0400

    update

--- Basic_data_structures/HB Challenges/Medium/9.24 Anagram of Palindrome.py
+++ Basic_data_structures/HB Challenges/Medium/9.24 Anagram of Palindrome.py
@@ -1,2 +1,30 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/anagram-of-palindrome/index.html#anagram-of-palindrome
 
+'''T/F if it's anagram of a palindrome'''
+
+# edge case: only alph
+# case insenstitive
+# 'a'--> true
+# 'ab' --> F
+# 'arceace' --> T
+# 'arceaceb' --> F
+
+def is_anagram_of_palindrome(word):
+    length = len(word)
+    count = {}
+    for char in word:
+        count[char] = count.get(char, 0) + 1
+    
+    seen_an_odd = False
+
+    for v in count.values():
+        if v % 2 != 0:
+            if seen_an_odd:
+                return False
+        seen_an_odd = True
+    return True
+
+
+words = ['a','ab','aab','arceace','arceaceb']
+for word in words:
+    print(is_anagram_of_palindrome(word))
