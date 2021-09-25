29d194bcf02ce36b102f9230a1e763a0b15d0da3
Kerrie Wong <wongkerrie@gmail.com>
2021-09-24 23:10:13 -0400

    update

--- Basic_data_structures/HB Challenges/Medium/9.24 Decode a String.py
+++ Basic_data_structures/HB Challenges/Medium/9.24 Decode a String.py
@@ -1,1 +1,15 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/decode/index.html#decode
+
+def decode(word):
+    res = []
+    for i, char in enumerate(word):
+        # print(char)
+        if char.isnumeric():
+            # print(i, int(char))
+            res.append(word[int(char)+i+1])
+    return ''.join(res)
+
+words = ["0h", "2abh", "0h1ae2bcy"]
+for word in words:
+    print(decode(word))
+    print('------')
