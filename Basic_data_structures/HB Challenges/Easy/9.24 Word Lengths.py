439873932c11ffe30f9fdbd4af602d3bae9e59b4
Kerrie Wong <wongkerrie@gmail.com>
2021-09-24 20:56:45 -0400

    update;

--- Basic_data_structures/HB Challenges/Easy/9.24 Word Lengths.py
+++ Basic_data_structures/HB Challenges/Easy/9.24 Word Lengths.py
@@ -1,1 +1,17 @@
-# https://fellowship.hackbrightacademy.com/materials/challenges/word-lengths/index.html#word-lengths
+# https://fellowship.hackbrightacademy.com/materials/challenges/word-lengths/index.html#word-lengths    
+
+'''give a phrase, return dict keyed by word-length, with the value of each length beingthe set of words of that length'''
+
+# dict within dict
+def word_lengths(phrase):
+    lengths = {}
+    words = phrase.split()
+
+    for word in words:
+        if len(word) not in lengths:
+            lengths[len(word)] = set()
+        lengths[len(word)].add(word)
+    return lengths
+
+phrase = "cute cats chase fuzzy rats"
+print(word_lengths(phrase))
