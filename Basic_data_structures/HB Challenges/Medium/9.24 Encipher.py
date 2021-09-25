c5cb927b4e6803034cee18b25a9d3281d0fc4fba
Kerrie Wong <wongkerrie@gmail.com>
2021-09-24 23:36:51 -0400

    update

--- Basic_data_structures/HB Challenges/Medium/9.24 Encipher.py
+++ Basic_data_structures/HB Challenges/Medium/9.24 Encipher.py
@@ -11,16 +11,15 @@ def cipher(phrase, shift):
     for letter in phrase:
         for i, char in enumerate(alpha):
             # print(letter)
-            if char.lower() == letter:
+            if char == letter.lower():
                 # print(alpha[i+shift])
-                print(letter)
+                # print(letter)
                 if letter.isupper():
-                    
                     res.append(alpha[i+shift].upper())    
                 else:
                     res.append(alpha[i+shift])
                 
     return ''.join(res)
 
-phrase = 'memeYUme'
+phrase = 'dsDFgf'
 print(cipher(phrase,4))
