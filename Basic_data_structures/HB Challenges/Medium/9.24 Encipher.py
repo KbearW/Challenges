878036336778bd2e5a6ff4258388749c3f10dbb2
Kerrie Wong <wongkerrie@gmail.com>
2021-09-24 23:34:03 -0400

    update

--- Basic_data_structures/HB Challenges/Medium/9.24 Encipher.py
+++ Basic_data_structures/HB Challenges/Medium/9.24 Encipher.py
@@ -1,2 +1,26 @@
 # https://fellowship.hackbrightacademy.com/materials/challenges/encipher-string/index.html#encipher-string
 
+# case senstitive, take in # and string
+
+# .isalpha()
+
+def cipher(phrase, shift):
+    alpha = 'abcdefghijklmopqrstuvwxyz'
+    res = []
+
+    for letter in phrase:
+        for i, char in enumerate(alpha):
+            # print(letter)
+            if char.lower() == letter:
+                # print(alpha[i+shift])
+                print(letter)
+                if letter.isupper():
+                    
+                    res.append(alpha[i+shift].upper())    
+                else:
+                    res.append(alpha[i+shift])
+                
+    return ''.join(res)
+
+phrase = 'memeYUme'
+print(cipher(phrase,4))
