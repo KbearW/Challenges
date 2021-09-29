caf25fd94687bb904c29b3375b4dc235ee43338f
Kerrie Wong <wongkerrie@gmail.com>
2021-09-28 20:15:43 -0400

    update

--- IK/Wk 1- Sorting/Video Coding/Brute Force Selection Sort/9.28 Bubble Sort.py
+++ IK/Wk 1- Sorting/Video Coding/Brute Force Selection Sort/9.28 Bubble Sort.py
@@ -1,2 +1,15 @@
 '''based on sudo code given, code the select sort algo out'''
 
+# Bubble sort: iterate over the array, then starts from R to L to find the smallest # and keep moving it alone
+# "Bubble up" in a way
+
+# This method is very similar to Selection sort in that selection sort scan from L -> R and direct swap
+# while bubble sort starts from R -> L and keep swapping the smallest item towards the L.
+
+# sudo code:
+    # for i in 0 to (n-1):
+    #     for red in (n-1) down to (i+1):
+    #         if A[red-1] > A[red]:
+    #             swap A[red-1], A[red]
+    # return A
+
