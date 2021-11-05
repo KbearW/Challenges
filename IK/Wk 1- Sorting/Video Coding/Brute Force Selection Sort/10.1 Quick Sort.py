1c53ff5aa872a39c8c04f3dd344fb55bf2906295
Kerrie Wong <wongkerrie@gmail.com>
2021-11-04 20:58:44 -0400

    update

--- IK/Wk 1- Sorting/Video Coding/Brute Force Selection Sort/10.1 Quick Sort.py
+++ IK/Wk 1- Sorting/Video Coding/Brute Force Selection Sort/10.1 Quick Sort.py
@@ -60,3 +60,35 @@ quicksort_lomuto(nums, 0, len(nums)-1)
     #         smaller ++, bigger --
     # swap A[start], A[bigger]
                     # <-- End of Hoare's code -->
+
+                    
+
+
+def quicksort_hoares(A):
+    ''' One pointer at the beginning of the list, one at the end'''
+
+    def helper(A, start, end):
+        if start >= end:
+            return
+
+        orange = start + 1
+        green = end
+        while orange <= green:
+            if A[orange] < A[start]:
+                orange += 1
+            elif A[green] > A[start]:
+                green -= 1
+            else: #both pointers stuck
+                A[orange], A[green] = A[green], A[orange]
+                orange += 1
+                green -= 1
+        A[start], A[green] = A[green], A[start]
+
+        helper(A, start, green -1)
+        helper(A, green +1, end)
+
+    helper(A, 0, len(A)-1)
+    return nums
+    
+nums = [4,3,9,10,2,6]
+quicksort_hoares(nums)
