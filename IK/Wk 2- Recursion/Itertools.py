01d01dc05465137f7074b72d3dff5ce41c05904b
Kerrie Wong <wongkerrie@gmail.com>
2021-11-25 15:28:41 -0500

    init

--- IK/Wk 2- Recursion/Itertools.py
+++ IK/Wk 2- Recursion/Itertools.py
@@ -1,15 +1,19 @@
 import itertools
 letter = 'ABCD'
 
+# This includes all possible pairs
 A = list(itertools.product(letter, repeat = 2))
 print(f'product is {len(A)} and {A}')
 
+# This removes dups - ie. (A,A), (B,B), (C,C), (D,D)
 B = list(itertools.permutations(letter, 2))
 print(f'permutations is  {len(B)} and {B}')
 
+# This removes all dups and order doesn't matter - ie. (A,A)... and (A,B)/(B,A) pair
 C = list(itertools.combinations(letter,2))
 print(f'combinations is  {len(C)} and {C}')
 
+# This includes all dups and order doesn't matter.
 D = list(itertools.combinations_with_replacement(letter,2))
 print(f'combinations_w_replacement is  {len(D)} and {D}')
 
