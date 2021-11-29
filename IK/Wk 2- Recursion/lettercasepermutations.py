e680b449937bcf7ed2cc5e314ab172a5ca481f99
Kerrie Wong <wongkerrie@gmail.com>
2021-11-28 23:25:38 -0500

    init

--- IK/Wk 2- Recursion/lettercasepermutations.py
+++ IK/Wk 2- Recursion/lettercasepermutations.py
@@ -1,3 +1,35 @@
+# This starts the fill from the left of the array.
+def function(array):
+    res = []
+    
+    def helper(i, temp, array):
+        # base case
+        if len(temp) == len(array):
+            res.append(''.join(temp))
+            print(f"line 8:{''.join(temp)[::-1]}")
+            # print(res)
+            return
+        else:
+            if array[i].isdigit():
+                temp.append(array[i])
+                # print(''.join(temp))
+                helper(i+1, temp, array)
+                temp.pop()
+            else:
+                temp.append(array[i].lower())
+                # print(temp)
+                helper(i+1, temp, array)
+                temp.pop()
+                
+                temp.append(array[i].upper())
+                # print(temp)
+                helper(i+1, temp, array)
+                temp.pop()
+        
+    helper(0, [], array)
+    return res
+print(function('a1b2'))
+# ['a1b2', 'a1B2', 'A1b2', 'A1B2']
 
 
 # This starts the fill from the right of the array.
@@ -31,3 +63,4 @@ def function(array):
     helper(len(array)-1, [], array)
     return res
 print(function('a1b2'))
+# ['a1b2', 'a1B2', 'A1b2', 'A1B2']
