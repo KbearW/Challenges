a5c033b837dc9ac05e855208a7063772b9f3dfd9
Kerrie Wong <wongkerrie@gmail.com>
2021-11-29 18:26:30 -0500

    init

--- IK/Wk 2- Recursion/DFS.py
+++ IK/Wk 2- Recursion/DFS.py
@@ -132,3 +132,51 @@ def permutations_wo_repetitions_str(n):
     return res
 permutations_wo_repetitions_str(['abc'])
 # ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+# Best space--> set temp/ slate as array and join since string is immutable and array is mutable.
+def permutations_wo_repetitions_str(n):
+    res = []
+    def bshelper(slate,n):
+        if len(n) == 0:
+            res.append(''.join(slate))
+        else:
+            for i in range(len(n)):
+                arr2 =  n[:i] + n[i+1:]
+                print(arr2)
+                bshelper(slate + str(n[i]), arr2)
+                # bshelper(slate + '1', n-1)
+    bshelper([], n)
+    print(res)
+    return res
+permutations_wo_repetitions_str(['abc'])
+# ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+''' This is the best solution for the classic permutation problem: instead of slicing the string, swap it to save space!'''
+def perm(words):
+    res = []
+    # This step is to split letters into individual string for easy access later
+    
+    words = [char for char in words]
+    
+    def helper(i, words, temp):
+        # Base case:
+        if len(words) == i:
+            res.append(''.join(temp))
+            return
+        
+        # Inner node
+        else:
+            for j in range(i, len(words)):
+                # swap with current string... in this example, when i = 1, words = 'bac' rather than 'abc'
+                words[i], words[j] = words[j], words[i]
+                # same logic here... append curr char to temp and pop it out when done
+                temp.append(words[i])
+                helper(i+1, words, temp)
+                temp.pop()
+                # swap back to original
+                words[j], words[i] = words[i], words[j]
+            
+    helper(0,words,[])
+    return res
+print(perm('abc'))
+# ['abc', 'acb', 'bac', 'bca', 'cba', 'cab']
