ba52fa319d22c0a91e115ab170958bc202a675d2
Kerrie Wong <wongkerrie@gmail.com>
2021-12-13 22:23:41 -0500

    update

---
+++ IK/Ad hoc- Strings/prime.py
@@ -0,0 +1,20 @@
+def prime(v):
+    res = []
+    # the num list should starts from 2 b/c 0, 1 are not prime!
+    
+    for i in range(2, v+1):
+        for j in range(2, i):
+        # for/else loop is need here to catch all things that fail
+            if(i % j == 0):
+                # Note: prime can only be div by 1 and itself, 
+                # so if the remainder is 0 for any division, 
+                # it's not a prime--> break
+                break
+        else:
+            res.append(i)
+        #     # break
+        #     # print(i, j)
+            # print(f'this is a prime {i}')
+            # break
+    return res
+print(prime(30))
